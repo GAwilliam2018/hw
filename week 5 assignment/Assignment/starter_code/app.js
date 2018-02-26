fc3e22de8fe0d7610b7a54594cd9439068a5322d
WILLIAM COKER <williamcoker@WILLIAMs-MacBook-Pro.local>
2018-02-25 21:32:52 -0500

    week 5 homework
    update

--- week 5 assignment/Assignment/starter_code/app.js
+++ week 5 assignment/Assignment/starter_code/app.js
@@ -19,6 +19,21 @@ else if (myCity =='san fransico' || myCity == 'sf' || myCity=='bay area'){
 	jQuery("body").attr('class','sf')
 }
 
+else if (myCity =='los angeles' || myCity == 'la' || myCity=='lax'){
+
+	jQuery("body").attr('class','la')
+}
+else if (myCity =='austin' || myCity == 'atx'){
+
+	jQuery("body").attr('class','austin')
+}
+
+else if (myCity =='sydney' || myCity == 'syd'){
+
+	jQuery("body").attr('class','sydney')
+}
+
+
 
  });
 
